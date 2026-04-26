482ab7c476f82a09943a5153f501815a1bfa2255
Deluxeg4 <vjhhgfv708@gmail.com>
2026-04-26 23:42:59 +0700

    Fix Gradle configuration, update dependencies, and fix Javadoc/Spotless issues

--- api/paper/build.gradle.kts
+++ api/paper/build.gradle.kts
@@ -11,11 +11,9 @@ tasks {
     javadoc {
         (options as StandardJavadocDocletOptions).run {
             encoding = Charsets.UTF_8.name()
-            links(
-                "https://jd.advntr.dev/api/${libs.versions.adventure.get()}/",
-                "https://jd.advntr.dev/text-minimessage/${libs.versions.adventure.get()}/",
-                "https://jd.papermc.io/paper/${libs.versions.paper.get().replace("-R0.1-SNAPSHOT", "")}/"
-            )
+            addStringOption("link", "https://jd.advntr.dev/api/${libs.versions.adventure.get()}/")
+            addStringOption("link", "https://jd.advntr.dev/text-minimessage/${libs.versions.adventure.get()}/")
+            addStringOption("link", "https://jd.papermc.io/paper/${libs.versions.paper.get().replace("-R0.1-SNAPSHOT", "")}/")
         }
     }  
 }
--- api/velocity/build.gradle.kts
+++ api/velocity/build.gradle.kts
@@ -9,13 +9,11 @@ dependencies {
 
 tasks {
     javadoc {
-        (options as StandardJavadocDocletOptions).run{
+        (options as StandardJavadocDocletOptions).run {
             encoding = Charsets.UTF_8.name()
-            links(
-                "https://jd.advntr.dev/api/${libs.versions.adventure.get()}/",
-                "https://jd.advntr.dev/text-minimessage/${libs.versions.adventure.get()}/",
-                "https://jd.papermc.io/velocity/${libs.versions.velocity.get().replace("-SNAPSHOT", "")}/"
-            )
+            addStringOption("link", "https://jd.advntr.dev/api/${libs.versions.adventure.get()}/")
+            addStringOption("link", "https://jd.advntr.dev/text-minimessage/${libs.versions.adventure.get()}/")
+            addStringOption("link", "https://jd.papermc.io/velocity/${libs.versions.velocity.get().replace("-SNAPSHOT", "")}/")
         }
     }
 }
